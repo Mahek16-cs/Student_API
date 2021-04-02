78c9a02f64221d9f40c9c35a86649e432bf300e7
Mahek Dembra <73291085+Mahek16-cs@users.noreply.github.com>
2021-04-02 23:27:48 +0500

    Add files via upload

---
+++ Models/model.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+let StudentSchema = new Schema({
+    name: {type: String, required: true, max: 100},
+    grade: {type: String, required: true}});
+    // Export the model     (model has to start with a capital letter in this case Product)
+    module.exports = mongoose.model('Student', StudentSchema);
+© 2021 GitHub, Inc.
